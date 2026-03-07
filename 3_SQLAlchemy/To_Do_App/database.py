97dee6c97f2e5fd575436c962ed3153bca122e36
sb13512 <sb13512@student.samk.fi>
2026-03-07 16:34:56 +0200

    Created boiler plate for the todo application

--- 3_SQLAlchemy/To_Do_App/database.py
+++ 3_SQLAlchemy/To_Do_App/database.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, mapped_column, Mapped
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, mapped_column, Mapped, relationship
 from pathlib import Path
+from typing import List
+from datetime import datetime
 
 DB_PATH = Path("/home/david_salome/mygitfolder/Database_Projects/3_SQLAlchemy/To_Do_App/todo.db")
 
@@ -9,11 +11,26 @@ engine = create_engine(f"sqlite+pysqlite:///{DB_PATH}", echo=False)
 class Base(DeclarativeBase):
     pass
 
-class Item(Base):
+class User(Base):
+    __tablename__ = "users"
+    
+    user_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    
+    items: Mapped[List["Todo_Item"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+class Todo_Item(Base):
     __tablename__ = "items"
     
     item_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(nullable=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now, nullable=True)
+    
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="items")
+    
     
 Base.metadata.create_all(engine)
 Session = sessionmaker(engine)
--- 3_SQLAlchemy/To_Do_App/main.py
+++ 3_SQLAlchemy/To_Do_App/main.py
@@ -3,59 +3,24 @@ import sys
 from database import *
 from sqlalchemy import select
 
+def register_user():
+    pass
+
+def login_user():
+    pass
+
+def todo_menu(user: User):
+    def view_items():
+        pass
+    def add_item():
+        pass
+    def delete_item():
+        pass
+    def logout():
+        pass
+
 def app():
-    print("Welcome to To-Do App! version 1.0")
-    while True:
-        print('''
-              What do you want to do today?
-              1. View todo items
-              2. Create new todo item
-              3. Remove item
-              4. Exit
-              ''')
-        
-        selection = input()
-        
-        if selection == "1": show_items()
-        elif selection == "2": create_new_item()
-        elif selection == "3": remove_item()
-        elif selection == "4": sys.exit("Goodbye!")
-
-def show_items():
-    print("Here are all your items: ")
-    with Session() as session:
-        stmt = select(Item)
-        items = session.scalars(stmt).all()
-        if len(items)<=0:
-            print("No items to show")
-        else:
-            for item in items:
-                print(f"{item.item_id}: {item.name}")
-
-def create_new_item():
-    print("Name for the item:")
-    name = input()
-    with Session() as session:
-        new_item = Item(name=name)
-        session.add(new_item)
-        session.commit()
-        
-        
-def remove_item():
-    print("Give ID to remove item:")
-    id = input()
-    with Session() as session:
-        stmt = select(Item).where(Item.item_id == id)
-        item = session.scalar(stmt)
-        if item is None:
-            print("No such item")
-        else:
-            session.delete(item)
-            session.commit()
-            print(f"Item with id {id} removed")
+    pass
 
 def main():
     app()
-    
-if __name__ == "__main__":
-    main()
