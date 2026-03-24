b054aaad31a5c2c04434ae13cbaa3947979646d1
sb13512 <sb13512@student.samk.fi>
2026-03-24 18:58:37 +0200

    Did the data transformation for the silver layer for cust_info table

---
+++ 5_sql_server_warehouse/scripts/silver/silver_ddl.sql
@@ -0,0 +1,76 @@
+-- DataWarehouse.silver.crm_cust_info definition
+
+-- Drop table
+
+-- DROP TABLE DataWarehouse.silver.crm_cust_info;
+
+-- If we want to create or replace the table
+
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
+CREATE TABLE DataWarehouse.silver.crm_cust_info (
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),      
+    cst_create_date DATE,
+	dwh_create_date DATETIME DEFAULT GETDATE() -- This is metadata added for our own reference for each table this will tell when this table was created
+);
+
+
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE DataWarehouse.silver.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME,
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+IF OBJECT_ID ('silver.crm_sales_detail', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_detail;
+CREATE TABLE DataWarehouse.silver.crm_sales_detail(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_id INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101;
+CREATE TABLE DataWarehouse.silver.erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
+CREATE TABLE DataWarehouse.silver.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+CREATE TABLE DataWarehouse.silver.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
