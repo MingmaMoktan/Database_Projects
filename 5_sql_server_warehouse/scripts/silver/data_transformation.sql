514bcb64377515f9f1b5dc17d6478b965eeed596
sb13512 <sb13512@student.samk.fi>
2026-07-29 17:47:46 +0300

    Added the silver.erp_cust_az12 clean data from bronze layer.

--- 5_sql_server_warehouse/scripts/silver/data_transformation.sql
+++ 5_sql_server_warehouse/scripts/silver/data_transformation.sql
@@ -353,13 +353,9 @@ END AS cid,
 CASE WHEN bdate>GETDATE() THEN NULL
      ELSE bdate
 END AS bdate,
-CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-     WHEN UPPER(TRIM(gen)) IN ('M', 'Male') THEN 'Male'
-     ELSE gen
+CASE WHEN UPPER(gen) LIKE 'F%' THEN 'Female'
+     WHEN UPPER(gen) LIKE 'M%' THEN 'Male'
+     ELSE 'n/a'
 END AS gen
 FROM 
 bronze.erp_cust_az12; -- This will check if the bdate is too much or the bdate is in future
-
-
-
-
